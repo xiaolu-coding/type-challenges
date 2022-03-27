588df46b4d957bd11f015e07c5029777690e6c8c
xiaolu-coding <454819375@qq.com>
2022-03-27 19:14:58 +0800

    chore: 14-easy-first

--- type-challenges/14-easy-first/README.md
+++ type-challenges/14-easy-first/README.md
@@ -1,9 +1,36 @@
 实现一个通用First<T>，它接受一个数组T并返回它的第一个元素的类型。
 
 例如：
-
+```typescript
 type arr1 = ['a', 'b', 'c']
 type arr2 = [3, 2, 1]
 
 type head1 = First<arr1> // expected to be 'a'
 type head2 = First<arr2> // expected to be 3
+```
+
+解决方法:
+
+> 根据给的例子, 得出T接收数组类型， T extends any[]
+
+使用js方法实现
+
+```javascript
+function First(arr) {
+  // 如果数组长度为0 返回never
+  if(arr.length === 0) {
+    return 'never'
+  } else {
+    // 返回第一个值
+    return arr[0]
+  }
+}
+```
+
+将js步骤转换为ts <font color=pink>arr为T extends any[]</font>
+
+```
+1. 判断arr是否是空数组 使用extends判断 ====> T extends []
+2. 如果是空数组，返回never   ======> T extends [] ? never
+3. 如果不是空数组，返回第一个值   ======> T extends [] ? never : T[0]
+```
--- type-challenges/14-easy-first/template.ts
+++ type-challenges/14-easy-first/template.ts
@@ -1,11 +1,30 @@
-// type First<T extends any[]> = T extends [] ? never : T[0]
+type First<T extends any[]> = T extends [] ? never : T[0]
 // type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
-type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
+// type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
 
 // const first = (arr) => {
 //   // arr 是不是一个空数组 如果是的话 那么返回 never
 //   return arr[0]
 // }
+
+// 首先使用js来实现
+// function First(arr) {
+//   // 如果数组长度为0 返回never
+//   if(arr.length === 0) {
+//     return 'never'
+//   } else {
+//     // 返回第一个值
+//     return arr[0]
+//   }
+// }
+
+// 从给的例子和文案来看，T接收一个数组 ====> T extends any[]
+
+// 将js转换成ts  
+//! 注: arr为 T extends any[] 
+// 1. 判断arr是否是空数组 使用extends判断 ====> T extends []
+// 2. 如果是空数组，返回never   ======> T extends [] ? never
+// 3. 如果不是空数组，返回第一个值   ======> T extends [] ? never : T[0]
 
 // 1.extends 类型条件判断
 // 2.获取 tuple 的 length 属性 indexed
