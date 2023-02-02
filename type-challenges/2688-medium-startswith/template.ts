5298dd29041e34322ce16026a54621b5c28e7997
xiaolu-coding <454819375@qq.com>
2023-02-02 09:04:23 +0800

    chore: 模式匹配做提取 startWith EndsWith

--- type-challenges/2688-medium-startswith/template.ts
+++ type-challenges/2688-medium-startswith/template.ts
@@ -1,3 +1,5 @@
 type StartsWith<T extends string, U extends string> = T extends `${U}${string}`
   ? true
   : false
+
+// 确保T U都是string类型，然后用T去匹配一个模式类型，模式类型的前缀是第二个U，后面为任意字符串
--- type-challenges/2693-medium-endswith/template.ts
+++ type-challenges/2693-medium-endswith/template.ts
@@ -1,1 +1,3 @@
 type EndsWith<T extends string, U extends string> = T extends `${string}${U}` ? true : false
+
+// 确保T U都是string类型，然后用T去匹配一个模式类型，模式类型的前缀是第二个U，前面为任意字符串
