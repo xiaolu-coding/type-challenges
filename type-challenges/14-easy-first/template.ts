1c454bf45b420fdbb0fa8c6f548358794a77834b
xiaolu-coding <454819375@qq.com>
2023-02-02 08:49:37 +0800

    feat: 模式匹配做提取的first

--- type-challenges/14-easy-first/template.ts
+++ type-challenges/14-easy-first/template.ts
@@ -1,4 +1,5 @@
-type First<T extends any[]> = T extends [] ? never : T[0]
+type First<T extends unknown[]> = T extends [infer R, ...unknown[]] ? R : never;
+// type First<T extends any[]> = T extends [] ? never : T[0]
 // type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
 // type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
 
