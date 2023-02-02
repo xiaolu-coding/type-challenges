9501c918afd5a71b67d9a76ff79aa05a1c9763fa
xiaolu-coding <454819375@qq.com>
2023-02-02 08:51:48 +0800

    feat: 模式匹配做提取 last

--- type-challenges/15-medium-last/template.ts
+++ type-challenges/15-medium-last/template.ts
@@ -1,3 +1,4 @@
 
-type Last<T extends any[]> = T extends [...any[], infer R] ? R : never
+// type Last<T extends any[]> = T extends [...any[], infer R] ? R : never
+type Last<T extends unknown[]> = T extends [...unknown[], infer R] ? R : never
 
