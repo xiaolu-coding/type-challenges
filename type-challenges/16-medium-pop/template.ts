49045832180f9ab4c4f5fb34e009b2d8800ae71d
xiaolu-coding <454819375@qq.com>
2023-02-02 08:57:05 +0800

    chore: 模式匹配做提取 pop shift

--- type-challenges/16-medium-pop/template.ts
+++ type-challenges/16-medium-pop/template.ts
@@ -1,4 +1,6 @@
-type Pop<T extends any[]> = T extends [...infer P, infer R] ? [...P] : never
+// type Pop<T extends any[]> = T extends [...infer P, infer R] ? [...P] : never
+type Pop<T extends unknown[]> = T extends [] ? [] : T extends [...infer P, infer R] ? [...P] : never
 
-type Shift<T extends any[]> = T extends [infer R, ...any[]] ? R : never
+// type Shift<T extends any[]> = T extends [infer R, ...any[]] ? R : never
+type Shift<T extends unknown[]> = T extends [] ? [] : T extends [infer R, ...unknown[]] ? R : never
 
