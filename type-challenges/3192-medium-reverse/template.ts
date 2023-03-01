349c84101f749f21343ae053cec71001f7336b93
xiaolu-coding <454819375@qq.com>
2023-03-01 14:38:18 +0800

    chore: 递归复用做循环

--- type-challenges/3192-medium-reverse/template.ts
+++ type-challenges/3192-medium-reverse/template.ts
@@ -1,3 +1,7 @@
 type Reverse<T extends unknown[]> = T extends [infer Pre, ...infer Rest]
   ? [...Reverse<Rest>, Pre]
   : []
+
+type REverseArr<T extends unknown[]> = T extends [infer First, ...infer Rest]
+  ? [...Reverse<Rest>, First]
+  : []
