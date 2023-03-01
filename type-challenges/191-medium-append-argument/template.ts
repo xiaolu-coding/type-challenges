2b19c02977f041304fe8a5eca1653a2e8c2beab0
xiaolu-coding <454819375@qq.com>
2023-03-01 10:40:56 +0800

    chore: 重新构造 191 AppendArgument

--- type-challenges/191-medium-append-argument/template.ts
+++ type-challenges/191-medium-append-argument/template.ts
@@ -1,1 +1,5 @@
-type AppendArgument<Fn, A> = Fn extends (...args: infer Args) => infer Res ? (...args: [...Args, A]) => Res : never
+// type AppendArgument<Fn, A> = Fn extends (...args: infer Args) => infer Res ? (...args: [...Args, A]) => Res : never
+
+type AppendArgument<Fn extends Function, Arg> =
+  Fn extends (...args: infer Args) => infer Res
+    ? (...args: [...Args, Arg]) => Res : never
