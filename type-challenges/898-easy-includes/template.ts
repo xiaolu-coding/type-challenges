61dce763c0644c4ccc65715bc7ebab10f9fc5f19
xiaolu-coding <454819375@qq.com>
2023-03-01 14:50:49 +0800

    chore: 递归复用做循环 includes

--- type-challenges/898-easy-includes/template.ts
+++ type-challenges/898-easy-includes/template.ts
@@ -1,5 +1,13 @@
-type Includes<T extends  any[], U> =  U extends T ? true: false
+type Includes<Arr extends unknown[], FindItem> = Arr extends [
+  infer FirstItem,
+  ...infer Rest
+]
+  ? isEqual<FirstItem, FindItem> extends true
+    ? true
+    : Includes<Rest, FindItem>
+  : false
 
+type isEqual<A, B> = (A extends B ? true : false) & (B extends A ? true : false)
 
 // 使用js来实现
 // function Includes(arr: any[], target: any): boolean {
@@ -7,8 +15,7 @@ type Includes<T extends  any[], U> =  U extends T ? true: false
 // }
 
 // 将js转换为ts
-//! 注: arr为 T extends any[], target为 U  
+//! 注: arr为 T extends any[], target为 U
 // 1. 遍历判断arr是否包含target   ======> U extends T
 // 2. 如果包含，返回true ======> U extends T ? true
 // 3. 如果不包含，返回false ======> U extends T ? true : false
-
