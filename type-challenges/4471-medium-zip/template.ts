34b304a983575e6091d38f115b777831b5d24216
xiaolu-coding <454819375@qq.com>
2023-03-01 10:02:16 +0800

    chore: 重新构造 Zip

--- type-challenges/4471-medium-zip/template.ts
+++ type-challenges/4471-medium-zip/template.ts
@@ -1,9 +1,15 @@
-type Zip<
-  T extends unknown[],
-  U extends unknown[],
-  Res extends unknown[] = []
-> = T extends [infer TF, ...infer TRest]
-  ? U extends [infer UF, ...infer URest]
-    ? Zip<TRest, URest, [...Res, [TF, UF]]>
-    : Res
-  : Res
+// type Zip<
+//   T extends unknown[],
+//   U extends unknown[],
+//   Res extends unknown[] = []
+// > = T extends [infer TF, ...infer TRest]
+//   ? U extends [infer UF, ...infer URest]
+//     ? Zip<TRest, URest, [...Res, [TF, UF]]>
+//     : Res
+//   : Res
+
+type Zip<One extends unknown[], Other extends unknown[]> = 
+  One extends [infer OneFirst, ...infer OneRest] 
+    ? Other extends [infer OtherFirst, ...infer OtherRest]
+      ? [[OneFirst, OtherFirst], ...Zip<OneRest, OtherRest>] : []
+      : []
