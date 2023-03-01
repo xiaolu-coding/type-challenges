1652fa4a012e4cbb9116d71722fed74ddda7f761
xiaolu-coding <454819375@qq.com>
2023-03-01 10:07:07 +0800

    chore: 重新构造 capitalize

--- type-challenges/110-medium-capitalize/template.ts
+++ type-challenges/110-medium-capitalize/template.ts
@@ -1,1 +1,3 @@
-type MyCapitalize<S extends string> = S extends `${infer R}${infer T}` ? `${Uppercase<R>}${T}` : ''
+// type MyCapitalize<S extends string> = S extends `${infer R}${infer T}` ? `${Uppercase<R>}${T}` : ''
+
+type MyCapitalize<Str extends string> =  Str extends `${infer First}${infer Rest}` ? `${Uppercase<First>}${Rest}` : Str
