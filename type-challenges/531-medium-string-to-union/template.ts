cad7e7bc9acbb59826a2a0d50f208134b8808183
xiaolu-coding <454819375@qq.com>
2023-03-02 09:30:41 +0800

    chore: 递归复用做循环 StringToUnion

--- type-challenges/531-medium-string-to-union/template.ts
+++ type-challenges/531-medium-string-to-union/template.ts
@@ -1,6 +1,11 @@
-type StringToUnion<
-  T extends string,
-  Result = never
-> = T extends `${infer Pre}${infer Post}`
-  ? StringToUnion<Post, Pre | Result>
-  : Result
+// type StringToUnion<
+//   T extends string,
+//   Result = never
+// > = T extends `${infer Pre}${infer Post}`
+//   ? StringToUnion<Post, Pre | Result>
+//   : Result
+
+type StringToUnion<Str extends string> =
+  Str extends `${infer First}${infer Rest}`
+    ? First | StringToUnion<Rest>
+    : never
