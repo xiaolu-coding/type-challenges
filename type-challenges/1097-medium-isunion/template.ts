fb8c306eb8b71495b62a97b14830302f88e1bf04
xiaolu-coding <454819375@qq.com>
2023-03-02 15:28:51 +0800

    chore: 联合分散可简化 IsUnion

--- type-challenges/1097-medium-isunion/template.ts
+++ type-challenges/1097-medium-isunion/template.ts
@@ -1,1 +1,8 @@
-type IsUnion<T, S = T> = T extends S ? [S] extends [T] ? false : true: false
+// type IsUnion<T, S = T> = T extends S ? [S] extends [T] ? false : true: false
+
+type IsUnion<A, B = A> = 
+  A extends A
+    ? [B] extends [A]
+      ? false
+      : true
+    : never
