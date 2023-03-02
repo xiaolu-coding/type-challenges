ca643f4416c33f8738af43b9746442484dd2149a
xiaolu-coding <454819375@qq.com>
2023-03-02 10:02:21 +0800

    chore: 递归复用做循环 reverseStr

--- type-challenges/3192-medium-reverse/template.ts
+++ type-challenges/3192-medium-reverse/template.ts
@@ -5,3 +5,8 @@ type Reverse<T extends unknown[]> = T extends [infer Pre, ...infer Rest]
 type REverseArr<T extends unknown[]> = T extends [infer First, ...infer Rest]
   ? [...Reverse<Rest>, First]
   : []
+
+type ReverseStr<Str extends string, Results extends string = ''> = 
+  Str extends `${infer First}${infer Rest}`
+    ? ReverseStr<Rest, `${First}${Results}`>
+    : Str
