bc0935c7e17f438c076f92fe43498e2e29ddc0ee
xiaolu-coding <454819375@qq.com>
2023-03-02 10:21:44 +0800

    chore: 数组长度做计数 Add

--- type-challenges/BuildArray/template.ts
+++ type-challenges/BuildArray/template.ts
@@ -2,3 +2,8 @@ type BuildArray<Length extends number, Element = unknown, Arr extends unknown[] 
   Arr['length'] extends Length
     ? Arr
     : BuildArray<Length, Element, [...Arr, Element]>
+
+type Add<Num1 extends number, Num2 extends number> = 
+  [...BuildArray<Num1>, ...BuildArray<Num2>]['length']
+
+type a = Add<52, 3>
