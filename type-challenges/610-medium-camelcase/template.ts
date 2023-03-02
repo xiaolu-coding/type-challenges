67f20063c073473cf8ced2f178fe34fb8d7bc43c
xiaolu-coding <454819375@qq.com>
2023-03-02 15:14:05 +0800

    chore: 联合分散可简化 CamelcaseUnion

--- type-challenges/610-medium-camelcase/template.ts
+++ type-challenges/610-medium-camelcase/template.ts
@@ -3,3 +3,8 @@ type CamelCase<S> = S extends `${infer L}-${infer T}${infer R}`
     ? `${L}-${CamelCase<`${T}${R}`>}`
     : `${L}${Uppercase<T>}${CamelCase<R>}`
   : S
+
+type CamelcaseUnion<Item extends string> =
+  Item extends `${infer Left}_${infer Right}${infer Rest}`
+    ? `${Left}${Uppercase<Right>}${CamelcaseUnion<Rest>}`
+    : Item
