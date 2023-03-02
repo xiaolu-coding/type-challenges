34d59a60fddd407e9538d73218843857ec888538
xiaolu-coding <454819375@qq.com>
2023-03-02 10:31:19 +0800

    chore: 数组长度做计数 Mutiply

--- type-challenges/BuildArray/template.ts
+++ type-challenges/BuildArray/template.ts
@@ -1,15 +1,31 @@
-type BuildArray<Length extends number, Element = unknown, Arr extends unknown[] = []> =
-  Arr['length'] extends Length
-    ? Arr
-    : BuildArray<Length, Element, [...Arr, Element]>
+type BuildArray<
+  Length extends number,
+  Element = unknown,
+  Arr extends unknown[] = []
+> = Arr["length"] extends Length
+  ? Arr
+  : BuildArray<Length, Element, [...Arr, Element]>
 
-type Add<Num1 extends number, Num2 extends number> = 
-  [...BuildArray<Num1>, ...BuildArray<Num2>]['length']
+type Add<Num1 extends number, Num2 extends number> = [
+  ...BuildArray<Num1>,
+  ...BuildArray<Num2>
+]["length"]
 
-type Subtract<Num1 extends number, Num2 extends number> =
-  BuildArray<Num1> extends [...arr1: BuildArray<Num2>, ...arr2: infer Rest]
-    ? Rest['length']
-    : never
+type Subtract<
+  Num1 extends number,
+  Num2 extends number
+> = BuildArray<Num1> extends [...arr1: BuildArray<Num2>, ...arr2: infer Rest]
+  ? Rest["length"]
+  : never
+
+type Mutiply<
+  Num1 extends number,
+  Num2 extends number,
+  ResultArr extends unknown[] = []
+> = Num2 extends 0
+  ? ResultArr["length"]
+  : Mutiply<Num1, Subtract<Num2, 1>, [...BuildArray<Num1>, ...ResultArr]>
 
 type a = Add<52, 3>
 type b = Subtract<52, 3>
+type c = Mutiply<3, 6>
