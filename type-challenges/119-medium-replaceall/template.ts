6c1b0417ae006660ee46bbbb1e9f46f4b2d4cbdf
xiaolu-coding <454819375@qq.com>
2023-03-02 09:19:20 +0800

    chore: 递归复用做循环 ReplaceAll

--- type-challenges/119-medium-replaceall/template.ts
+++ type-challenges/119-medium-replaceall/template.ts
@@ -1,10 +1,14 @@
-type ReplaceAll<
-  S extends string,
-  From extends string,
-  To extends string
-> = S extends `${infer Pre}${From}${infer Post}`
-  ? From extends ""
-    ? S
-    : `${Pre}${To}${ReplaceAll<`${Post}`, From, To>}`
-  : S
+// type ReplaceAll<
+//   S extends string,
+//   From extends string,
+//   To extends string
+// > = S extends `${infer Pre}${From}${infer Post}`
+//   ? From extends ""
+//     ? S
+//     : `${Pre}${To}${ReplaceAll<`${Post}`, From, To>}`
+//   : S
 
+type ReplaceAll<Str extends string, From extends string, To extends string> =
+  Str extends `${infer Left}${From}${infer Right}`
+    ? `${Left}${To}${ReplaceAll<Right, From, To>}`
+    : Str
