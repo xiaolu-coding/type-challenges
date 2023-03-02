240674bf01d5bd7e707b5b7f3d0344f557301c2d
xiaolu-coding <454819375@qq.com>
2023-03-02 15:36:39 +0800

    chore: Bem

--- type-challenges/3326-medium-bem-style-string/template.ts
+++ type-challenges/3326-medium-bem-style-string/template.ts
@@ -1,7 +1,14 @@
+// type BEM<
+//   B extends string,
+//   E extends string[],
+//   M extends string[]
+// > = `${B}${E extends [] ? "" : `__${E[number]}`}${M extends []
+//   ? ""
+//   : `--${M[number]}`}`
+
+
 type BEM<
-  B extends string,
-  E extends string[],
-  M extends string[]
-> = `${B}${E extends [] ? "" : `__${E[number]}`}${M extends []
-  ? ""
-  : `--${M[number]}`}`
+  Block extends string,
+  Element extends string[],
+  Modifiers extends string[]
+> = `${Block}__${Element[number]}--${Modifiers[number]}`
